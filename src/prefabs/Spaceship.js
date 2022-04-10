0a693cad52b4afb445433cd049ddbd8fd377e8b6
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-09 22:43:59 -0700

    Create Spaceship.js
    Code for the spaceship targets.

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,17 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);   //adds to existing scene
+        this.points = pointValue;   //stores the point value
+        this.moveSpeed = 3;         //pixels per frame
+    }
+
+    update() {
+        //move spaceship left
+        this.x -= this.moveSpeed;
+        //wrap around
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        }
+    }
+}
